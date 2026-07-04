0f021a6cb24983379e5d3f4423cdb8e8e0aefe13
root <root@srv1573174.hstgr.cloud>
2026-07-04 21:18:25 +0000

    feat: Sentry error monitoring added

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'virtualcare-ng-v10';
+const CACHE_VERSION = 'virtualcare-ng-v13';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 
